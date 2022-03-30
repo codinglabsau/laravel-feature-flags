b2fb25ac1f63e6af95fccfe43a8321fb97ad3c1a
stevethomas <steve@codinglabs.io>
2022-03-30 16:13:18 +1000

    refactor to match()

--- src/FeatureFlagsServiceProvider.php
+++ src/FeatureFlagsServiceProvider.php
@@ -11,11 +11,6 @@ class FeatureFlagsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-feature-flags')
             ->hasConfigFile()
