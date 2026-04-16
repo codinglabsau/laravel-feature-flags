5d426d8a58729eedd752c79fb5b8eb8b2cffe30d
stevethomas <steve@codinglabs.com.au>
2026-04-16 16:30:23 +1000

    Revert "Remove getScope and getDescription facade methods"
    This reverts commit 13b658b8e0d42df529b0ac5f28e1a390ad6739de.

--- tests/FeatureFlagTest.php
+++ tests/FeatureFlagTest.php
@@ -222,6 +222,26 @@ it('can update a features state', function () {
         ->and(cache()->store('array')->get('testing.some-feature'))->toBe(FeatureState::on()->value);
 });
 
+it('returns the scope for a feature', function () {
+    Feature::factory()->create([
+        'name' => 'some-feature',
+        'state' => FeatureState::off(),
+        'scope' => 'development',
+    ]);
+
+    expect(FeatureFlag::getScope('some-feature'))->toBe('development');
+});
+
+it('returns null scope when feature has no scope', function () {
+    Feature::factory()->create([
+        'name' => 'some-feature',
+        'state' => FeatureState::off(),
+        'scope' => null,
+    ]);
+
+    expect(FeatureFlag::getScope('some-feature'))->toBeNull();
+});
+
 it('filters features by scope using model scope', function () {
     Feature::factory()->create(['name' => 'dev-feature', 'state' => FeatureState::off(), 'scope' => 'development']);
     Feature::factory()->create(['name' => 'release-feature', 'state' => FeatureState::off(), 'scope' => 'release']);
@@ -229,6 +249,26 @@ it('filters features by scope using model scope', function () {
 
     expect(Feature::scope('development')->pluck('name')->all())->toBe(['dev-feature'])
         ->and(Feature::scope('release')->pluck('name')->all())->toBe(['release-feature']);
+});
+
+it('returns the description for a feature', function () {
+    Feature::factory()->create([
+        'name' => 'some-feature',
+        'state' => FeatureState::off(),
+        'description' => 'New search powered by Meilisearch',
+    ]);
+
+    expect(FeatureFlag::getDescription('some-feature'))->toBe('New search powered by Meilisearch');
+});
+
+it('returns null description when feature has no description', function () {
+    Feature::factory()->create([
+        'name' => 'some-feature',
+        'state' => FeatureState::off(),
+        'description' => null,
+    ]);
+
+    expect(FeatureFlag::getDescription('some-feature'))->toBeNull();
 });
 
 it('uses the default cache store when cache store has not been set', function () {
